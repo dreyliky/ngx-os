9dd705c6ba7eeda2fdf3dd479748045061cd27cc
dreyliky <dreyliky@gmail.com>
2020-06-17 00:17:55 +0300

    ng-os

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-windows app is running!');
+    expect(page.getTitleText()).toEqual('ng-os app is running!');
   });
 
   afterEach(async () => {
