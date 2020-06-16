0b7ff58f2863be6a51d781105e38165ff389d23f
dreyliky <dreyliky@gmail.com>
2020-06-17 00:13:13 +0300

    LegacyThemes folders; Win98 actual theme folder with independent files;

--- projects/os-angular/src/lib/modules/checkbox/checkbox.component.ts
+++ projects/os-angular/src/lib/modules/checkbox/checkbox.component.ts
@@ -11,12 +11,23 @@ export class CheckboxComponent implements OnInit {
     @Input()
     public label: string;
 
-    public id: string;
+    @Input()
+    public set id (value: string) {
+        this._id = value;
+    }
+
+    public get id (): string {
+        return this._id;
+    }
+
+    private _id: string;
 
     constructor () {}
 
     public ngOnInit (): void {
-        this.generateId();
+        if (!this._id) {
+            this.generateId();
+        }
     }
 
     private generateId (): void {
