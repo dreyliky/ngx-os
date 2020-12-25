33229a48bf4fe340955716db2429d6e8da5a5280
DreyLiky <dreyliky@gmail.com>
2020-12-25 19:36:58 +0200

    Dynamic theme changing

--- projects/os-angular/src/lib/themes/theme.service.ts
+++ projects/os-angular/src/lib/themes/theme.service.ts
@@ -12,18 +12,18 @@ export class ThemeService {
 
     public applyTheme(themeName: string): void {
         const head = this.document.getElementsByTagName('head')[0];
-
         const themeLink = this.document.getElementById(
             'client-theme'
         ) as HTMLLinkElement;
+        const themeHref = `${themeName}.css`;
 
         if (themeLink) {
-            themeLink.href = themeName;
+            themeLink.href = themeHref;
         } else {
             const style = this.document.createElement('link');
             style.id = 'client-theme';
             style.rel = 'stylesheet';
-            style.href = `${themeName}.css`;
+            style.href = themeHref;
 
             head.appendChild(style);
         }
--- src/app/layouts/components/component-header/component-header.component.ts
+++ src/app/layouts/components/component-header/component-header.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ThemeArray } from '@Doc/core/data';
 import { DocumentationRouteEnum, ThemeEnum } from '@Doc/core/enums';
 import { ThemeManagerService } from '@Doc/core/services';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'demo-component-header',
@@ -12,20 +13,35 @@ import { ThemeManagerService } from '@Doc/core/services';
 export class ComponentHeaderComponent implements OnInit {
 
     public themes = ThemeArray;
+    public appliedTheme: ThemeEnum;
 
     public routeEnum = DocumentationRouteEnum;
 
+    private appliedThemeSubscription: Subscription;
+
     constructor(
+        private readonly changeDetector: ChangeDetectorRef,
         private readonly themeManagerService: ThemeManagerService
     ) {}
 
-    public ngOnInit(): void {}
+    public ngOnInit(): void {
+        this.initAppliedThemeObserver();
+    }
 
     public onThemeChanged(event: Event): void {
         const target = event.target as HTMLSelectElement;
         const themeName = target.value as ThemeEnum;
 
         this.themeManagerService.applyTheme(themeName);
+    }
+
+    private initAppliedThemeObserver(): void {
+        this.appliedThemeSubscription = this.themeManagerService.appliedTheme$
+            .subscribe((theme) => {
+                this.appliedTheme = theme;
+
+                this.changeDetector.detectChanges();
+            });
     }
 
 }
