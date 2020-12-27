0776d41765c88335a7fb076c7abe0cd12bd47519
DreyLiky <dreyliky@gmail.com>
2020-12-27 16:28:31 +0200

    Counter app for button overview component

--- src/app/features/doc/demo/button/button-overview/button-overview.component.ts
+++ src/app/features/doc/demo/button/button-overview/button-overview.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'demo-button-overview',
     templateUrl: './button-overview.component.html',
+    styleUrls: ['./button-overview.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonOverviewComponent implements OnInit {
 
+    public counter: number = 0;
+
     constructor() {}
 
     public ngOnInit(): void {}
+
+    public onIncreaseButtonClick(): void {
+        this.counter++;
+    }
+
+    public onDecreaseButtonClick(): void {
+        this.counter--;
+    }
 
 }
