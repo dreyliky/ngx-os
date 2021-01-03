0b84b893558f59f36978894e520f966953a6b844
DreyLiky <dreyliky@gmail.com>
2021-01-03 12:17:36 +0200

    Selectbox option refactoring

--- projects/os-angular/src/lib/modules/selectbox/components/option/option.component.ts
+++ projects/os-angular/src/lib/modules/selectbox/components/option/option.component.ts
@@ -6,7 +6,22 @@ import { OsBaseComponent } from 'os-angular/core';
 @Component({
     selector: 'os-option',
     templateUrl: './option.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        'class': 'os-element os-option',
+        '[class.selected]': 'selected',
+        '[class]': 'styleClass',
+        '[id]': 'id',
+        '[ngStyle]': 'style',
+        '(click)': 'onOptionClick($event)',
+        '(dblclick)': 'osDblclick.emit($event)',
+        '(mousedown)': 'osMousedown.emit($event)',
+        '(mousemove)': 'osMousemove.emit($event)',
+        '(mouseout)': 'osMouseout.emit($event)',
+        '(mouseover)': 'osMouseover.emit($event)',
+        '(mouseup)': 'osMouseup.emit($event)',
+        '(wheel)': 'osWheel.emit($event)'
+    }
 })
 export class OptionComponent<T> extends OsBaseComponent {
 
