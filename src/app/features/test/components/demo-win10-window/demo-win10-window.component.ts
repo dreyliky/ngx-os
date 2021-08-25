ed707af14643c490b9b7639bb39edb20bc396b03
dreyliky <dreyliky@gmail.com>
2021-08-26 00:24:28 +0300

    (fix) visual improvements

--- src/app/features/test/components/demo-win10-window/demo-win10-window.component.ts
+++ src/app/features/test/components/demo-win10-window/demo-win10-window.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TreeNode } from 'os-angular';
 
 @Component({
     selector: 'app-demo-win10-window',
@@ -7,22 +6,4 @@ import { TreeNode } from 'os-angular';
     styleUrls: ['./demo-win10-window.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DemoWin10WindowComponent {
-    public treeItems: TreeNode<any>[] = [
-        { label: 'Item 1' },
-        {
-            label: 'Item 2',
-            children: [
-                {
-                    label: 'Child item 1',
-                    children: [
-                        { label: 'Another child element 1' },
-                        { label: 'Another child element 2' }
-                    ]
-                },
-                { label: 'Child item 2' }
-            ]
-        },
-        { label: 'Item 3' }
-    ];
-}
+export class DemoWin10WindowComponent {}
