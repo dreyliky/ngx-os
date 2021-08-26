cf6b68022b8e75fadec01a3029f9493869aa5556
dreyliky <dreyliky@gmail.com>
2021-08-26 15:17:50 +0300

    (fix) winxp bugfixes

--- projects/os-angular/src/lib/modules/button/components/button/button.component.ts
+++ projects/os-angular/src/lib/modules/button/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit } from '@angular/core';
 import { OsBaseButtonComponent } from '@lib-core';
 import { buttonType } from '../../shared';
 
@@ -10,6 +10,9 @@ import { buttonType } from '../../shared';
 export class ButtonComponent extends OsBaseButtonComponent implements OnInit {
     @Input()
     public type: buttonType = 'button';
+
+    @HostBinding('attr.tabindex')
+    protected readonly hostTabindexAttr = 0;
 
     public ngOnInit(): void {
         this.classlistManager.add('os-button');
