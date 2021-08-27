3e55ba451128887ec7494d84f5dbcbdc18aa02a9
dreyliky <dreyliky@gmail.com>
2021-08-27 23:38:34 +0300

    (fix) main page features description

--- src/app/pages/main/blocks/features/features.component.ts
+++ src/app/pages/main/blocks/features/features.component.ts
@@ -17,19 +17,19 @@ export class FeaturesComponent {
         {
             iconUrl: '/assets/icons/components.png',
             title: '10+ COMPONENTS',
-            description: 'Functional and easy to use components help you to build'
+            description: 'Functional and easy to use components help you to build '
                         + 'an amazing app in operating system style.'
         },
         {
             iconUrl: '/assets/icons/themes.png',
             title: '3 THEMES',
-            description: 'Try to use: Win98, WinXP, Win10 themes.'
+            description: 'Try to use: Win98, WinXP, Win10 themes. '
                         + 'You can switch between in realtime.'
         },
         {
             iconUrl: '/assets/icons/open-source.png',
             title: 'OPEN SOURCE',
-            description: 'All components are open source and free to use under MIT license.'
+            description: 'All components are open source and free to use under MIT license. '
                         + 'Feel the power of open source.'
         }
     ];
