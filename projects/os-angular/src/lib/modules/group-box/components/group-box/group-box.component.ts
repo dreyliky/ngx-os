641e31dffda37bf90c1c621a38458a7d4b8f413c
dreyliky <dreyliky@gmail.com>
2021-09-09 19:39:48 +0300

    (fix) group-box & form-field

--- projects/os-angular/src/lib/modules/group-box/components/group-box/group-box.component.ts
+++ projects/os-angular/src/lib/modules/group-box/components/group-box/group-box.component.ts
@@ -17,6 +17,7 @@ export class GroupBoxComponent extends OsBaseComponent implements OnInit {
     }
 
     public ngOnInit(): void {
+        this.classlistManager.add('os-group-box');
         this.initElementEventObservers(this.hostElementRef.nativeElement);
     }
 }
