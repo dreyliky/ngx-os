75839bc79ecfe365d9e55b7418313991a6cefe10
dreyliky <dreyliky@gmail.com>
2021-09-15 22:26:10 +0300

    (docs) dynamic window ref interface

--- projects/os-angular/src/lib/modules/window/interfaces/dynamic-window-ref.interface.ts
+++ projects/os-angular/src/lib/modules/window/interfaces/dynamic-window-ref.interface.ts
@@ -5,26 +5,46 @@ import { IDynamicWindowParams } from './dynamic-window-params.interface';
 
 /** Public API of DynamicWindowRef */
 export interface IDynamicWindowRef {
+    /** Is the dynamic window hidden */
     isHidden$: Observable<boolean>;
+    /** Is the dynamic window at full-screen */
     isFullscreen$: Observable<boolean>;
+    /** Is the dynamic window active (means the user works with it right now) */
     isActive$: Observable<boolean>;
-    afterClosed$: Observable<any>;
+    /** Configuration object for the dynamic window */
     config$: Observable<IDynamicWindowParams>;
+    /** Emits value when the dynamic window closed */
+    afterClosed$: Observable<any>;
 
+    /** Id of the dynamic window */
     id: string;
+    /** Is the dynamic window hidden */
     isHidden: boolean;
+    /** Is the dynamic window at full-screen */
     isFullscreen: boolean;
+    /** Is the dynamic window active (means the user works with it right now) */
     isActive: boolean;
+    /** Configuration object for the dynamic window */
     config: IDynamicWindowParams;
+    /** The HTML element of the dynamic window */
     windowElement: HTMLElement;
+    /** The component rendered inside the dynamic window */
     componentRef: ComponentRef<DynamicWindowComponent>;
 
+    /** Changes the configuration of the dynamic window */
     updateConfig(config: IDynamicWindowParams): void;
+    /** Hides dynamic window */
     hide(): void;
+    /** Shows dynamic window */
     show(): void;
+    /** Makes dynamic window visible or hidden */
     toggleVisibility(state: boolean): void;
+    /** Maximizes the dynamic window at full-screen mode */
     goFullscreen(): void;
+    /** Minimizes the dynamic window at windowed mode */
     goWindowed(): void;
+    /** Makes dynamic window full-screen or windowed mode */
     toggleFullscreen(state: boolean): void;
+    /** Closes the dynamic window */
     close<T>(result?: T): void;
 }
