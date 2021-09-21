a45cc5fa15367b49ce04ad341017af1b04fb1bc1
dreyliky <dreyliky@gmail.com>
2021-09-21 18:02:45 +0300

    (refactor) scroll-view

--- projects/ngx-os/src/lib/modules/scroll-view/components/scroll-view/scroll-view.component.ts
+++ projects/ngx-os/src/lib/modules/scroll-view/components/scroll-view/scroll-view.component.ts
@@ -8,31 +8,33 @@ import { OsBaseComponent } from '@lib-core';
 })
 export class ScrollViewComponent extends OsBaseComponent implements OnInit {
     @Input()
-    public isVerticalScrollEnabled = true;
+    public isVerticalScrollEnabled: boolean = true;
 
     @Input()
-    public isHorizontalScrollEnabled = false;
+    public isHorizontalScrollEnabled: boolean = false;
+
+    @Input()
+    public isVerticalScrollHiding: boolean = false;
+
+    @Input()
+    public isHorizontalScrollHiding: boolean = false;
 
     @HostBinding('style.overflow-x')
-    public get hostOverflowX(): string {
-        const overrideValue = this.style?.['overflow'] || this.style?.['overflowX'];
-
-        if (!overrideValue) {
+    public get _hostOverflowX(): string {
+        if (!this.isHorizontalScrollHiding) {
             return (this.isHorizontalScrollEnabled) ? 'auto' : 'hidden';
         }
 
-        return overrideValue;
+        return (this.isHorizontalScrollEnabled) ? 'scroll' : 'hidden';
     }
 
     @HostBinding('style.overflow-y')
-    public get hostOverflowY(): string {
-        const overrideValue = this.style?.['overflow'] || this.style?.['overflowY'];
-
-        if (!overrideValue) {
+    public get _hostOverflowY(): string {
+        if (!this.isVerticalScrollHiding) {
             return (this.isVerticalScrollEnabled) ? 'auto' : 'hidden';
         }
 
-        return overrideValue;
+        return (this.isVerticalScrollEnabled) ? 'scroll' : 'hidden';
     }
 
     constructor(
