fce03fd6f5d82dfbcaefa1b7a6108c78b9976ac3
dreyliky <dreyliky@gmail.com>
2021-10-02 19:44:20 +0300

    (refactor) move code block to shared

--- src/app/showcase/pages/components/overview/examples/examples.module.ts
+++ src/app/showcase/pages/components/overview/examples/examples.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared';
-import { CodeComponent } from './code';
 import { ExampleComponent } from './example';
 import { ExamplesComponent } from './examples.component';
 
 @NgModule({
     declarations: [
         ExamplesComponent,
-        ExampleComponent,
-        CodeComponent
+        ExampleComponent
     ],
     imports: [
         SharedModule
--- src/app/showcase/shared/shared.module.ts
+++ src/app/showcase/shared/shared.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgxOsModule } from 'ngx-os';
+import { CodeComponent } from './components';
 
 @NgModule({
-    exports: [
+    declarations: [
+        CodeComponent
+    ],
+    imports: [
         CommonModule,
         RouterModule,
         HttpClientModule,
         FormsModule,
         ReactiveFormsModule,
         NgxOsModule
+    ],
+    exports: [
+        CommonModule,
+        RouterModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgxOsModule,
+
+        CodeComponent
     ]
 })
 export class SharedModule {}
