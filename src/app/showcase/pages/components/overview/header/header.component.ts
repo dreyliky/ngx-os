4e886c13c53b2e59d4c663f887946bd6f29f0026
dreyliky <dreyliky@gmail.com>
2021-10-05 15:47:47 +0300

    (feature) source code button in component overview

--- src/app/showcase/pages/components/overview/header/header.component.ts
+++ src/app/showcase/pages/components/overview/header/header.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { ComponentMetaInfo } from '@features/documentation';
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { OverviewService } from '../overview.service';
 
 @Component({
@@ -9,14 +9,34 @@ import { OverviewService } from '../overview.service';
     styleUrls: ['./header.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent implements OnInit {
-    public metaInfo$: Observable<ComponentMetaInfo>;
+export class HeaderComponent implements OnInit, OnDestroy {
+    public metaInfo: ComponentMetaInfo;
+
+    public get sourceCodeLink(): string {
+        return `https://github.com/dreyliky/ngx-os/tree/master/src/app/library/modules/${this.metaInfo.type}`;
+    }
+
+    private metaInfoSubscription: Subscription;
 
     constructor(
-        private readonly overviewService: OverviewService
+        private readonly overviewService: OverviewService,
+        private readonly changeDetector: ChangeDetectorRef
     ) {}
 
     public ngOnInit(): void {
-        this.metaInfo$ = this.overviewService.metaInfo$;
+        this.initMetaInfoObserver();
+    }
+
+    public ngOnDestroy(): void {
+        this.metaInfoSubscription.unsubscribe();
+    }
+
+    private initMetaInfoObserver(): void {
+        this.metaInfoSubscription = this.overviewService.metaInfo$
+            .subscribe((metaInfo) => {
+                this.metaInfo = metaInfo;
+
+                this.changeDetector.detectChanges();
+            });
     }
 }
