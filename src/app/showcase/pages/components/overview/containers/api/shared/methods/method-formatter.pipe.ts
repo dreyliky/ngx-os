939c60ef7bcbd0f761e08f352d51b8f6d665b0ac
dreyliky <dreyliky@gmail.com>
2021-10-20 20:30:20 +0300

    (feature) method-formatter type params support

--- src/app/showcase/pages/components/overview/containers/api/shared/methods/method-formatter.pipe.ts
+++ src/app/showcase/pages/components/overview/containers/api/shared/methods/method-formatter.pipe.ts
@@ -6,11 +6,29 @@ import { MethodsClass } from '@features/documentation';
 })
 export class MethodFormatterPipe implements PipeTransform {
     public transform(method: MethodsClass): string {
-        let output = this.addOptionalSymbol(method.name, method.optional);
+        let output = this.addTypeParameters(method.name, method);
+        output = this.addOptionalSymbol(output, method.optional);
         output = this.addBracketsWithParams(output, method);
         output = this.addReturnType(output, method);
 
         return output;
+    }
+
+    private addTypeParameters(output: string, method: MethodsClass): string {
+        let result = output;
+
+        if (method.typeParameters?.length) {
+            result += `<<span class="text-primary">`;
+
+            method.typeParameters?.forEach((param, paramIndex) => {
+                result += param;
+                result += (paramIndex < (method.typeParameters.length - 1)) ? ', ' : '';
+            });
+
+            result += '</span>>';
+        }
+
+        return result;
     }
 
     private addOptionalSymbol(output: string, isOptional: boolean): string {
