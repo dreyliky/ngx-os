3479950fea598667eaed83a10fe570f19b347392
dreyliky <dreyliky@gmail.com>
2022-03-14 21:39:19 +0200

    (fix) window with custom drag&drop logic example

--- src/app/showcase/features/documentation/examples/window/window-dynamic-custom-dragging-logic/window-dynamic-custom-dragging-logic.component.ts
+++ src/app/showcase/features/documentation/examples/window/window-dynamic-custom-dragging-logic/window-dynamic-custom-dragging-logic.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import {
+    DragInfo,
     DragStrategyByAxisProperties,
     DynamicWindowRef,
     DynamicWindowService,
     DYNAMIC_WINDOW_REF
 } from 'ngx-os';
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'showcase-window-dynamic-custom-dragging-logic',
@@ -51,15 +53,15 @@ class MyAppComponent implements OnInit, OnDestroy {
 
     public ngOnInit(): void {
         this.initWindowDragStartObserver();
-        // this.initWindowDragEndObserver();
-        // this.windowRef.updateConfig({
-        //     draggerConfig: {
-        //         mouseMoveHandler: (event) => {
-        //             this.calculateWindowPositionX(event);
-        //             this.calculateWindowPositionY(event);
-        //         }
-        //     }
-        // });
+        this.initWindowDragEndObserver();
+        this.windowRef.updateConfig({
+            draggerConfig: {
+                mouseMoveHandler: (event) => {
+                    this.calculateWindowPositionX(event);
+                    this.calculateWindowPositionY(event);
+                }
+            }
+        });
     }
 
     public ngOnDestroy(): void {
@@ -68,58 +70,84 @@ class MyAppComponent implements OnInit, OnDestroy {
     }
 
     private initWindowDragStartObserver(): void {
-        // this.windowRef.draggableDirective.osDragStart
-        //     .pipe(takeUntil(this.viewDestroy$))
-        //     .subscribe(({ originalEvent, movableElement }) => {
-        //         this.shiftX = (originalEvent.clientX - movableElement.offsetLeft);
-        //         this.shiftY = (originalEvent.clientY - movableElement.offsetTop);
-        //     });
+        this.windowRef.draggableDirective.osDragStart
+            .pipe(takeUntil(this.viewDestroy$))
+            .subscribe(({ originalEvent, movableElement }) => {
+                this.shiftX = (PointerHelper.getClientX(originalEvent) - movableElement.offsetLeft);
+                this.shiftY = (PointerHelper.getClientY(originalEvent) - movableElement.offsetTop);
+            });
     }
 
-    // private initWindowDragEndObserver(): void {
-    //     this.windowRef.draggableDirective.osDragEnd
-    //         .pipe(takeUntil(this.viewDestroy$))
-    //         .subscribe(({ movableElement }) => this.onDragEnd(movableElement));
-    // }
+    private initWindowDragEndObserver(): void {
+        this.windowRef.draggableDirective.osDragEnd
+            .pipe(takeUntil(this.viewDestroy$))
+            .subscribe(({ movableElement }) => this.onDragEnd(movableElement));
+    }
 
-    // private calculateWindowPositionX(
-    //     { originalEvent: { clientX }, movableElement }: DragInfo
-    // ): void {
-    //     const axisPropertyName = this.dragStrategy.xAxisLeftStyleProperty;
+    private calculateWindowPositionX(
+        { originalEvent, movableElement }: DragInfo
+    ): void {
+        const clientX = PointerHelper.getClientX(originalEvent);
+        const axisPropertyName = this.dragStrategy.xAxisLeftStyleProperty;
 
-    //     if (this.shiftX > clientX) {
-    //         movableElement.style.setProperty(axisPropertyName, '0px');
-    //     } else if ((clientX - this.shiftX + movableElement.clientWidth) < innerWidth) {
-    //         movableElement.style.setProperty(axisPropertyName, `${clientX - this.shiftX}px`);
-    //     } else {
-    //         const coordinate = `${innerWidth - movableElement.clientWidth}px`;
+        if (this.shiftX > clientX) {
+            movableElement.style.setProperty(axisPropertyName, '0px');
+        } else if ((clientX - this.shiftX + movableElement.clientWidth) < innerWidth) {
+            movableElement.style.setProperty(axisPropertyName, `${clientX - this.shiftX}px`);
+        } else {
+            const coordinate = `${innerWidth - movableElement.clientWidth}px`;
 
-    //         movableElement.style.setProperty(axisPropertyName, coordinate);
-    //     }
-    // }
+            movableElement.style.setProperty(axisPropertyName, coordinate);
+        }
+    }
 
-    // private calculateWindowPositionY(
-    //     { originalEvent: { clientY }, movableElement }: DragInfo
-    // ): void {
-    //     const axisPropertyName = this.dragStrategy.yAxisTopStyleProperty;
+    private calculateWindowPositionY(
+        { originalEvent, movableElement }: DragInfo
+    ): void {
+        const clientY = PointerHelper.getClientY(originalEvent);
+        const axisPropertyName = this.dragStrategy.yAxisTopStyleProperty;
 
-    //     if (this.shiftY > clientY) {
-    //         movableElement.style.setProperty(axisPropertyName, '0px');
-    //     } else if ((clientY - this.shiftY + movableElement.clientHeight) < innerHeight) {
-    //         movableElement.style.setProperty(axisPropertyName, `${clientY - this.shiftY}px`);
-    //     } else {
-    //         const coordinate = `${innerHeight - movableElement.clientHeight}px`;
+        if (this.shiftY > clientY) {
+            movableElement.style.setProperty(axisPropertyName, '0px');
+        } else if ((clientY - this.shiftY + movableElement.clientHeight) < innerHeight) {
+            movableElement.style.setProperty(axisPropertyName, `${clientY - this.shiftY}px`);
+        } else {
+            const coordinate = `${innerHeight - movableElement.clientHeight}px`;
 
-    //         movableElement.style.setProperty(axisPropertyName, coordinate);
-    //     }
-    // }
+            movableElement.style.setProperty(axisPropertyName, coordinate);
+        }
+    }
 
-    // private onDragEnd(windowElement: HTMLElement): void {
-    //     if (
-    //         windowElement.offsetLeft <= 0 ||
-    //         (windowElement.offsetLeft + windowElement.clientWidth) >= innerWidth
-    //     ) {
-    //         this.windowRef.goFullscreen();
-    //     }
-    // }
+    private onDragEnd(windowElement: HTMLElement): void {
+        if (
+            windowElement.offsetLeft <= 0 ||
+            (windowElement.offsetLeft + windowElement.clientWidth) >= innerWidth
+        ) {
+            this.windowRef.goFullscreen();
+        }
+    }
+}
+
+export abstract class PointerHelper {
+    public static getClientX(event: PointerEvent | TouchEvent): number {
+        if (this.isPointerEvent(event)) {
+            return event.clientX;
+        }
+
+        return event.changedTouches[0].clientX;
+    }
+
+    public static getClientY(event: PointerEvent | TouchEvent): number {
+        if (this.isPointerEvent(event)) {
+            return event.clientY;
+        }
+
+        return event.changedTouches[0].clientY;
+    }
+
+    public static isPointerEvent(event: Event): event is PointerEvent {
+        const mouseEvent = event as PointerEvent;
+
+        return (mouseEvent.clientX !== undefined) && (mouseEvent.clientY !== undefined);
+    }
 }
