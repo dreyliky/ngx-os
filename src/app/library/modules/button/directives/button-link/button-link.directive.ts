b570e1a716fb851784919332cf3bb5b2eb11ce96
dreyliky <dreyliky@gmail.com>
2023-04-01 18:03:25 +0300

    (docs): button module

--- src/app/library/modules/button/directives/button-link/button-link.directive.ts
+++ src/app/library/modules/button/directives/button-link/button-link.directive.ts
@@ -1,5 +1,6 @@
 import { Directive } from '@angular/core';
 
+/** Transforms HTML button to link-button in os style */
 @Directive({
     selector: 'button[osButtonLink]',
     host: {
--- src/app/library/modules/button/directives/button/button.directive.ts
+++ src/app/library/modules/button/directives/button/button.directive.ts
@@ -1,5 +1,6 @@
 import { Directive } from '@angular/core';
 
+/** Transforms HTML button to button in os style */
 @Directive({
     selector: 'button[osButton]',
     host: {
